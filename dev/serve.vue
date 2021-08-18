514554028976a397693b3452f047cc9bfc3552a8
Ryan Harmon <heyharmon@gmail.com>
2021-08-18 11:49:57 -0600

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { VueDesignSystemSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  VueDesignSystemSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-design-system-sample />
+  </div>
+</template>
